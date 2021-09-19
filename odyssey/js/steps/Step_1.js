4e1cda159f0b8aa76c5f70d15214d1651d21367a
Vanessa <vanessa.bar@celsius-online.com>
2021-09-19 19:37:23 +0200

    MàJ des fichiers

--- odyssey/js/steps/Step_1.js
+++ odyssey/js/steps/Step_1.js
@@ -2,19 +2,32 @@
 	const step = { 
 		name: `Troie`, 
 		markerOrientation: `left`,
-		summary: `Les Grecs ont enfin gagné cette interminable guerre de Troie. Entrés dans la ville grâce à la ruse d’Ulysse, ils la détruisent, la pillent, tuent certains habitants et ramènent les autres, prisonniers, en trophée. Cependant, une partie des Grecs oublient de remercier les dieux par des cadeaux, des offrandes et sont punis : ils devront errer des années. Pour Ulysse, les vents sont capricieux et soufflent dans le mauvais sens.`,
+		summary: `Les Grecs ont enfin gagné cette interminable guerre de Troie. Entrés dans la ville grâce à la ruse d’Ulysse, ils la détruisent,
+		 la pillent, tuent certains habitants et ramènent les autres, prisonniers, en trophée. Cependant, une partie des Grecs oublient de remercier 
+		 les dieux par des cadeaux, des offrandes et sont punis : ils devront errer des années. Pour Ulysse, les vents sont capricieux et soufflent 
+		 dans le mauvais sens.`,
 		stories: [
 			{ 
 				title: `Un, deux, Troie ?`, 
-				text: `On estime que la cité de Troie a bien existé, sous le nom de Troia (Τροία) ou d’Illios (Ἴλιος) d’où le nom de l’épopée racontant la guerre de Troie, l’Illiade. On la situe en Turquie actuelle, où on peut encore visiter un vaste site archéologique.  Cependant, il est difficile de différencier ce qui dépend de l’Histoire (c’est-à-dire ce qui a vraiment existé) et ce qui n’est que pure légende. La guerre de Troie telle qu’elle est racontée n’a probablement pas eu lieu, c’est un mythe peut-être néanmoins inspiré de faits réels. Dans la mythologie, les remparts de la ville ont été construits par les dieux Poséidon et Apollon. Ils sont donc imprenables.`
+				text: `<p>On estime que la cité de Troie a bien existé, sous le nom de Troia (Τροία) ou d’Illios (Ἴλιος) d’où le nom de l’épopée 
+				racontant la guerre de Troie, l’<i>Illiade</i>. On la situe en Turquie actuelle, où on peut encore visiter un vaste site archéologique.  
+				Cependant, il est difficile de différencier ce qui dépend de l’Histoire (c’est-à-dire ce qui a vraiment existé) et ce qui n’est 
+				que pure légende. La guerre de Troie telle qu’elle est racontée n’a probablement pas eu lieu, c’est un mythe peut-être néanmoins 
+				inspiré de faits réels. Dans la mythologie, les remparts de la ville ont été construits par les dieux Poséidon et Apollon. 
+				Ils sont donc imprenables.</p>`
 			},
 			{ 
 				title: `Un héros en cache un autre.`, 
-				text: `Enée, héros troyen juste et brave, réussit à fuir la ville en portant sur son dos son vieux père Anchise. Il deviendra à son tour le héros d'une épopée extrêmement célèbre écrite par le poète romain Virgile, l'Enéide. Dans cette épopée, Enée traverse lui aussi la Méditerranée et fonde une dynastie dont sont issus les célèbres Romulus et Rémus, les fondateurs mythiques de la ville de Rome.`
+				text: `<p><b>Enée</b>, héros troyen juste et brave, réussit à fuir la ville en portant sur son dos son vieux père <b>Anchise</b>. Il deviendra 
+				à son tour le héros d'une épopée extrêmement célèbre écrite par le poète romain <b>Virgile</b>, l'<i>Enéide</i>. Dans cette épopée, <b>Enée</b> traverse 
+				lui aussi la Méditerranée et fonde une dynastie dont sont issus les célèbres <b>Romulus</b> et <b>Rémus</b>, les fondateurs mythiques de la ville de Rome.</p>`
 			},
 			{
 				title: `Un accueil mortel`,
-				text: `Pour Agamemnon, le voyage de retour est beaucoup plus court. Son cousin Egisthe lui a organisé un banquet pour fêter son retour. Hélas, Agamemnon ignore qu’Egisthe l’a remplacé auprès de son épouse, la reine Clytemnestre. Egisthe assassine le roi et prend sa place. Egisthe et Clytemnestre seront ensuite eux-mêmes tués par le fils d’Agamemnon, Oreste. On a beaucoup représenté, au théâtre et dans la littérature, les crimes de cette famille, qu’on appelle les “Atrides”.`
+				text: `<p>Pour <b>Agamemnon</b>, le voyage de retour est beaucoup plus court. Son cousin <b>Egisthe</b> lui a organisé un banquet pour fêter son retour. 
+				Hélas, <b>Agamemnon</b> ignore qu’<b>Egisthe</b> l’a remplacé auprès de son épouse, la reine <b>Clytemnestre</b>. Egisthe assassine le roi et prend sa place. 
+				Egisthe et Clytemnestre seront ensuite eux-mêmes tués par le fils d’Agamemnon, Oreste. On a beaucoup représenté, au théâtre et dans 
+				la littérature, les crimes de cette famille, qu’on appelle les “<b>Atrides</b>”.</p>`
 			}
 		]
 	};
--- odyssey/js/steps/Step_3.js
+++ odyssey/js/steps/Step_3.js
@@ -2,12 +2,18 @@
 	const step = { 
 		name: `Île de Cythère`, 
 		markerOrientation: `left`,
-		summary: `Repartis d’Ismaros, Ulysse et ses hommes se rapprochent petit à petit d’Ithaque. Hélas, le vent se lève encore. C’est Borée, le dieu du vent du Nord, qui se met en colère et souffle de tous ses poumons. Ulysse souhaite que sa flotte contourne l’île grecque de Cythère pour remonter jusqu’à Ithaque, mais le vent l’en empêche et la conduit droit devant, à toute vitesse. 
-Voilà qui complique bien les choses…`,
+		summary: `Repartis d’<b>Ismaros</b>, <b>Ulysse</b> et ses hommes se rapprochent petit à petit d’<b>Ithaque</b>. Hélas, le vent se lève encore. C’est <b>Borée</b>, le dieu du vent
+		 du Nord, qui se met en colère et souffle de tous ses poumons. Ulysse souhaite que sa flotte contourne l’île grecque de <b>Cythère</b> pour remonter jusqu’à
+		  Ithaque, mais le vent l’en empêche et la conduit droit devant, à toute vitesse. Voilà qui complique bien les choses…`,
 		stories: [
 			{ 
 				title: `Cythère, île de l’amour ?`, 
-				text: `Cythère est une petite île au sud de la Grèce continentale, dans la mer Egée. Dans la mythologie, c’est le lieu de naissance de la déesse de la beauté et de l’amour Aphrodite, sortie d’un coquillage. La naissance d’Aphrodite a été représentée à de nombreuses reprises. Ici, elle a été peinte par le peintre italien de la Renaissance Sandro Boticelli. Ce tableau extrêmement célèbre s’appelle <i>La naissance de Vénus</i>. Elle naît dans la mer, formée par les flots et transportée par un coquillage.`
+				text: `<p><b>Cythère</b> est une petite île au sud de la Grèce continentale, dans la mer <b>Egée</b>. Dans la mythologie, c’est le lieu de naissance
+				 de la déesse de la beauté et de l’amour Aphrodite, sortie d’un coquillage.</p>
+				 <img src="images/venus.jpg">
+				 <p>La naissance d’<b>Aphrodite</b> a été représentée à de nombreuses 
+				 reprises. Ici, elle a été peinte par le peintre italien de la Renaissance Sandro Boticelli. Ce tableau extrêmement célèbre s’appelle
+				  <i>La naissance de Vénus</i>. Elle naît dans la mer, formée par les flots et transportée par un coquillage.</p>`
 			}
 		] 
 	};
